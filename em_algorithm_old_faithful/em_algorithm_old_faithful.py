b495bb9feccecf1b6cac46959e770d08b7c1ea3e
five state solution <91334121+caryllwyd@users.noreply.github.com>
2024-09-05 19:55:08 +0200

    05/09/24 Evening

--- em_algorithm_old_faithful/em_algorithm_old_faithful.py
+++ em_algorithm_old_faithful/em_algorithm_old_faithful.py
@@ -27,36 +27,41 @@ def log_lik_mix_norm(data, mu, sigma, p):
     for i in range(n_comp):
         values[:, i] = p[i] * norm.pdf(data, mu[i], sigma[i])  # mixture of Gaussian distributions
 
-    return np.log(values.sum())
+    return np.log(values.sum()+1e-10)
 
 
 # EM iterative algorithm function
 
 def em_algorithm(data, mu_0, sigma_0, p_0):
-    n_comp = len(p_0)
-    n_data = len(data)
-    tau = np.zeros((n_data, n_comp)) 
-    mu_k = mu_0
-    sigma_k = sigma_0
-    p_k = p_0
-    f = 0
-    f_i = np.zeros((len(data_waiting),n_comp))
-    tau = np.zeros((len(data_waiting),n_comp))
-    print(f"abs likelihood: {np.abs(log_lik_mix_norm(data_waiting, mu_k, sigma_k, p_k))}")
-    #while(np.abs(log_lik_mix_norm(data_waiting, mu_k, sigma_k, p_k)) > 1): 
-    for i in range(n_comp):
-        f_i[:, i] = norm.pdf(data_waiting, mu_k[i], sigma_k[i]) # likelihood values
-    f = (f_i @ p_k).sum()
-    for i in range(n_comp):
-        tau[:, i] = p_k[i] * (f_i[:, i]/f)
-        p_k[i] = tau[:, i].sum()/n_data
-
-    mu_k = (tau.T @ data)/n_data   
-    print(f"f: {f}") 
-    print(f"abs. likelihood after update: {np.abs(log_lik_mix_norm(data_waiting, mu_k, sigma_k, p_k))}")
+    n_comp = len(p_0) # number of mixt. comps
+    n_data = len(data) # length of dataset
      
+    # initial values:
+    mu_k = mu_0[:] 
+    sigma_k = sigma_0[:]
+    p_k = p_0[:]
+    likef = np.zeros(n_data)
+    likef_i = np.zeros((n_data,n_comp))
+    tau = np.zeros((n_data,n_comp))
 
-    return mu_k, sigma_k, p_k, tau[1:5, :]
+    print(f"abs likelihood: {np.abs(log_lik_mix_norm(data_waiting, mu_k, sigma_k, p_k))}")
+
+    while(np.abs(log_lik_mix_norm(data, mu_k, sigma_k, p_k)) > 0.1): 
+        for i in range(n_comp):
+            likef_i[:, i] = norm.pdf(data, mu_k[i], sigma_k[i]) # likelihood values
+        for i in range(n_data):
+            likef[i] = likef_i[i, :] @ p_k
+        #print(f"f: {likef}")
+        for i in range(n_comp):
+            tau[:, i] = p_k[i] * (likef_i[:, i]/likef[:])
+            p_k[i] = tau[:, i].sum()/n_data
+        mu_k = (tau.T @ data)/n_data  
+        #sigma_k = (tau.T @ ((mu_k - data)^2))/n_data 
+        #print(f"tau: {tau}") 
+        print(f"abs. likelihood after update: {np.abs(log_lik_mix_norm(data, mu_k, sigma_k, p_k))}")
+        print(f"Mu_k: {mu_k}")
+
+    return mu_k, sigma_k, p_k, tau[0:5, :]
 
 
 
