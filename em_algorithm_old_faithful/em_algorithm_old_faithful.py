fbb851eb598dddd6703c2c039ab40d01332a6aa9
five state solution <91334121+caryllwyd@users.noreply.github.com>
2024-09-09 09:12:55 +0200

    09/09/24 Morning (w/h histogram)

--- em_algorithm_old_faithful/em_algorithm_old_faithful.py
+++ em_algorithm_old_faithful/em_algorithm_old_faithful.py
@@ -9,7 +9,7 @@ import os
 os.chdir('C:/Users/turla/Documents/GitHub/masters-thesis/em_algorithm_old_faithful')
 
 # Loading data
-file_path = 'old_faithful.csv'  # Replace with your file path
+file_path = 'em_old_faithful.csv'  # Replace with your file path
 data = pd.read_csv(file_path)
 
 data_waiting = data["waiting"]
@@ -25,7 +25,7 @@ def log_lik_mix_norm(data, mu, sigma, p):
     for i in range(n_comp):
         values[:, i] = p[i] * norm.pdf(data, mu[i], sigma[i])  # mixture of Gaussian distributions
 
-    return np.log(values.sum()+1e-10) # constant added for numerical reasons
+    return np.log(values.sum()) # constant added for numerical reasons
 
 
 
@@ -66,46 +66,12 @@ def m_step(expectation, data, p):
     p_hat = expectation.sum(axis=1)/n_data
     return mu_hat, sigma_hat, p_hat  
 
-    
-# EM iterative algorithm function
-def em_algorithm(data, mu_0, sigma_0, p_0):
-    n_comp = len(p_0) # number of mixt. comps
-    n_data = len(data) # length of dataset
-     
-    # initial values:
-    mu_k = mu_0[:] 
-    sigma_k = sigma_0[:]
-    p_k = p_0[:]
-    likef = np.zeros(n_data)
-    likef_i = np.zeros((n_data,n_comp))
-    tau = np.zeros((n_data,n_comp))
-
-    print(f"abs likelihood: {np.abs(log_lik_mix_norm(data_waiting, mu_k, sigma_k, p_k))}")
-
-    while(np.abs(log_lik_mix_norm(data, mu_k, sigma_k, p_k)) > 0.3): 
-        for i in range(n_comp):
-            likef_i[:, i] = norm.pdf(data, mu_k[i], sigma_k[i]) # likelihood values
-        for i in range(n_data):
-            likef[i] = likef_i[i, :] @ p_k
-        #print(f"f: {likef}")
-        for i in range(n_comp):
-            tau[:, i] = p_k[i] * (likef_i[:, i]/likef[:])
-            p_k[i] = tau[:, i].sum()/n_data
-        mu_k = (tau.T @ data)/n_data  
-        #sigma_k = (tau.T @ ((mu_k - data)^2))/n_data 
-        #print(f"tau: {tau}") 
-        print(f"abs. likelihood after update: {np.abs(log_lik_mix_norm(data, mu_k, sigma_k, p_k))}")
-        print(f"Mu_k: {mu_k}")
-
-    return mu_k, sigma_k, p_k, tau[0:5, :]
-
-
 # Number of components
 n_components = 2
 
 # Initialise parameters
 mu_0 = np.random.choice(data_waiting, n_components)
-sigma_0 = 1 + 2*np.random.random(n_components)
+sigma_0 = 1 + 10*np.random.random(n_components)
 p_0 = np.ones(n_components) / n_components  # n coefficients of the same value
 
 print(f"Initial means: {mu_0}")
@@ -114,24 +80,29 @@ print(f"Initial mixing coefficients: {p_0}")
 
 
 # Visualisation
-zeroes = np.zeros(len(data_waiting)) # for pdf's
-plt.scatter(data_waiting, data_eruptions, c="cyan", s=8, edgecolors="black")
+
+def visualise(mu, sigma, p, res, name):
+    n_comp = len(p)
+    plt.hist(data_waiting, color='skyblue', edgecolor='black', alpha = 0.5)
+    #plt.scatter(data_waiting, data_eruptions, c="cyan", s=8, edgecolors="black")
 
 # PDF graph
-xmin, xmax = plt.xlim()
-ymin, ymax = plt.ylim()
-x_vals = np.linspace(xmin, xmax, 100)
-y_1 = norm.pdf(x_vals, mu_0[0], sigma_0[0])
-y_2 = norm.pdf(x_vals, mu_0[1], sigma_0[1])
+    xmin, xmax = plt.xlim()
+    ymin, ymax = plt.ylim()
+    x_vals = np.linspace(xmin, xmax, res)
+    y = np.zeros(res)
+    for i in range(n_comp):
+        y += p[i]*norm.pdf(x_vals, mu[i], np.sqrt(sigma[i]))
 
-plt.plot(x_vals, ymax*y_1, linewidth=1, c="red")
-plt.plot(x_vals, ymax*y_2, linewidth=1, c="blue")
-plt.show()
+    plt.plot(x_vals, ymax / max(y) * y, linewidth=1, c="red")
+    plt.savefig(name)
+    plt.close()
 
 #e = e_step(data_waiting, mu, sigma, p)
 #m = m_step(e, data_waiting, p)
 
 psi1 = [mu_0, sigma_0, p_0]
+visualise(mu_0, sigma_0, p_0, 1000, "pics_latest_run/image0.png")
 like_diff, no_iter = 1, 0
 while(like_diff > 1e-5):
     no_iter += 1
@@ -140,8 +111,10 @@ while(like_diff > 1e-5):
     l1 = log_lik_mix_norm(data_waiting, psi1[0], psi1[1], psi1[2])
     l2 = log_lik_mix_norm(data_waiting, psi2[0], psi2[1], psi2[2])
     like_diff = abs(l2 - l1)
-    print(f"mu: {psi2[0]}")
+    if (no_iter % 5 == 0):
+        print(f"mu({no_iter}): {psi2[0]}, like: {log_lik_mix_norm(data_waiting, psi2[0], psi2[1], psi2[2])}")
     psi1 = [psi2[0], psi2[1], psi2[2]]
+    visualise(psi1[0], psi1[1], psi2[2], 100, f"pics_latest_run/image{no_iter}.png")
 
     
 #s = em_algorithm(data_waiting, mu, sigma, p)
